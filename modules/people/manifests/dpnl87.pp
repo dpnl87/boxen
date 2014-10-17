e5eaa8a306a37a8895fdb6d6934fb3fd16f5ce36
Daniel Paulus <d.paulus@gmail.com>
2014-10-17 14:30:20 +0200

    Add OSX defaults

--- modules/people/manifests/dpnl87.pp
+++ modules/people/manifests/dpnl87.pp
@@ -23,7 +23,7 @@ class people::dpnl87 {
   include vlc
 
   # Install custom vagrant plugins
-  vagrant::plugin { 'vagrant-cachier': }
+  #vagrant::plugin { 'vagrant-cachier': }
 
   # Install custom vagrant boxes
   vagrant::box { 'centos-6.5/virtualbox':
@@ -39,6 +39,7 @@ class people::dpnl87 {
     dotfiles => [
       'zshrc',
       'vimrc.after',
+      'zpreztorc',
     ],
     packages => {
       brew   => [
@@ -82,6 +83,66 @@ class people::dpnl87 {
     source_dir => $env['directories']['dotfiles'],
     dest_dir   => $env['directories']['home'],
   }
+
+  # OSX Defaults
+  boxen::osx_defaults { 'Expand save panel by default':
+    domain => 'NSGlobalDomain',
+    key    => 'NSNavPanelExpandedStateForSaveMode',
+    value  => true,
+  }
+  boxen::osx_defaults { 'Expand save panel by default 2':
+    domain => 'NSGlobalDomain',
+    key    => 'NSNavPanelExpandedStateForSaveMode2',
+    value  => true,
+  }
+
+  boxen::osx_defaults { 'Expand print panel by default':
+    domain => 'NSGlobalDomain',
+    key    => 'PMPrintingExpandedStateForPrint',
+    value  => true,
+  }
+  boxen::osx_defaults { 'Expand print panel by default 2':
+    domain => 'NSGlobalDomain',
+    key    => 'PMPrintingExpandedStateForPrint2',
+    value  => true,
+  }
+
+  boxen::osx_defaults { 'Save to disk (not to iCloud) by default':
+    domain => 'NSGlobalDomain',
+    key    => 'NSDocumentSaveNewDocumentsToCloud',
+    value  => true,
+  }
+
+  boxen::osx_defaults { 'Automatically quit printer app':
+    domain => 'com.apple.print.PrintingPrefs',
+    key    => 'Quit When Finished',
+    value  => true,
+  }
+
+  boxen::osx_defaults { 'Disable the “Are you sure you want to open this application?” dialog':
+    domain => 'com.apple.LaunchServices',
+    key    => 'LSQuarantine',
+    value  => false,
+  }
+
+  boxen::osx_defaults { 'Disable Resume system-wide':
+    domain => 'NSGlobalDomain',
+    key    => 'NSQuitAlwaysKeepsWindows',
+    value  => false,
+  }
+
+  boxen::osx_defaults { 'Reveal IP address, hostname, OS version, etc. when clicking the clock':
+    domain => '/Library/Preferences/com.apple.loginwindow',
+    key    => 'AdminHostInfo',
+    value  => 'HostName',
+  }
+
+  boxen::osx_defaults { 'Disable smart quotes':
+    domain => 'NSGlobalDomain',
+    key    => 'NSAutomaticQuoteSubstitutionEnabled',
+    value  => false,
+  }
+
 
   # Misc Helpers until I can figure out where to put this
   define dotfile::link($source_dir, $dest_dir) {
